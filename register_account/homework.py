ba27e358d208c4bdc14496b76bc8ed04374bd443
tanzidakhan <tanzidak12@gmail.com>
2025-07-15 18:22:37 +0600

    fake

--- register_account/homework.py
+++ register_account/homework.py
@@ -1,24 +1,35 @@
+
 import time
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from faker import Faker
+fake = Faker()
 
 driver = webdriver.Firefox()
 driver.maximize_window()
+
 driver.get("https://tutorialsninja.com/demo/index.php?route=account/register")
 first_name = driver.find_element(by=By.ID,value = "input-firstname")
-first_name.send_keys("Tanzida")
+first_name.send_keys(fake.first_name())
+
 last_name = driver.find_element(by=By.ID,value = "input-lastname")
-last_name.send_keys("Khan")
+last_name.send_keys(fake.last_name())
+
 email = driver.find_element(by=By.ID,value = "input-email")
-email.send_keys("tanzida@12344.com")
+email.send_keys(fake.email())
+
 telephone = driver.find_element(by=By.ID,value = "input-telephone")
-telephone.send_keys("01234567")
+telephone.send_keys(fake.phone_number())
+
 Password = driver.find_element(by=By.ID,value = "input-password")
 Password.send_keys("Html12344")
+
 Password_confirm = driver.find_element(by=By.ID,value = "input-confirm")
 Password_confirm.send_keys("Html12344")
+
 subscribe = driver.find_element(by=By.NAME,value = "newsletter")
 subscribe.click()
+
 privacy_policy = driver.find_element(by=By.NAME,value = "agree")
 subscribe.click()
 time.sleep(5)
